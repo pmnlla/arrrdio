ea481e75b5b839e8fddab5c37c15a0f150f63649
pmnlla <39027165+pmnlla@users.noreply.github.com.>
2025-04-29 23:03:43 -0700

    im making this shit work bro

---
+++ arrrdio-webui/app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const items = ref(['Spotify', 'AppleMusic', 'YTMusic', 'Soundcloud'])
+const value = ref('Spoify')
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <USelectMenu v-model="value" :items="items" class="w-48" />
+    <br>
+    <UFormField label="Link" description="We'll use this to queue up your song on the Arrrdio!">
+      <UInput placeholder="Enter song link:" class="w-full" />
+    </UFormField>
+  </div>
+</template>
