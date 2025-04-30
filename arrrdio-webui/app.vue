5a113bfe6d0ea8ff7bee7e38af9511ceb807ae43
pmnlla <39027165+pmnlla@users.noreply.github.com.>
2025-04-29 23:03:56 -0700

    im going to bite lily

--- arrrdio-webui/app.vue
+++ arrrdio-webui/app.vue
@@ -1,14 +1,29 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 const items = ref(['Spotify', 'AppleMusic', 'YTMusic', 'Soundcloud'])
 const value = ref('Spoify')
+
+async function call() {
+  const platform = ref('');
+  const req = new Request("https://ntfy.sh/arrrdiotests", {
+    method: "POST",
+    body: platform.value,
+    headers: {
+        'Content-Type': 'text/plain',
+      },
+  });
+  await $fetch(req);
+}
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center h-screen">
-    <USelectMenu v-model="value" :items="items" class="w-48" />
-    <br>
-    <UFormField label="Link" description="We'll use this to queue up your song on the Arrrdio!">
-      <UInput placeholder="Enter song link:" class="w-full" />
+  <div class="flex flex-col items-center justify-center h-screen gap-6">
+    <UFormField label="Platform" description="To ensure we get the correct information from your link!" class="w-96 flex items-center jutify-center">
+    <USelectMenu v-model="platform" :items="items" class="w-48" />
     </UFormField>
+    <UFormField label="Link" description="We'll use this to queue up your song on the Arrrdio!" class="w-96">
+      <UInput v-model="songLink" placeholder="Enter song link:" class="w-full" />
+    </UFormField>
+    <UButton @click="call" loading-auto trailing-icon="i-lucide-arrow-right" size="md" label="Set Sail!" />
   </div>
 </template>
